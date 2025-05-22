c44f1f1aac51d61912bb39d5b6f3ba7b320155bb
Chico204 <olatunjimark391@gmail.com>
2025-05-22 02:54:10 +0100

    done

--- src/accordion.jsx
+++ src/accordion.jsx
@@ -1,2 +1,35 @@
 import React, {useState} from 'react';
-import {Plus, Minus } from "lucide-react"
+import {Plus, Minus } from "lucide-react";
+import  faq  from './faq';
+
+export default function Accordion() {
+    const [activeIndex, setActiveIndex] = useState(null);
+    const toggleButton = (index) => {
+        setActiveIndex(prev => (prev === index ? null : index))
+    }
+  
+  return (
+    <div className=' mx-auto p-4'>
+        <h2 className='text-2xl mb-6 font-bold text-center'>Frequently Asked Questions</h2>
+   <div className=' space-y-4 max-w-2xl mx-auto'>
+    {faq.map((faq, index)=> (
+      <div key={index} className= 'border rounded-lg'>
+          <button className='w-full flex justify-between items-center p-4 ' onClick = {()=> toggleButton(index)} >
+            <span>{faq.question}</span>
+            {activeIndex === index ? (<Minus className='w-5 h-5 text-indigo-600'/>) : (<Plus className='w-5 h-5 text-indigo-600'/>)}
+            </button>    
+           {activeIndex === index && (
+            <div className='px-4 pb-4 text-gray-700'>
+                {faq.answer}
+            </div>
+           )}
+      </div>  
+
+    ))}
+   </div>
+   
+    </div>
+
+  )
+}
+
---
+++ src/faq.jsx
@@ -0,0 +1,26 @@
+//faq.jsx
+
+const faq = [
+    {
+        question: "Do I have to allow the use of cookies?",
+        answer: "If you want to use the website, you have to allow the use of cookies. We use cookies to make our website work properly and to improve your experience."
+    },
+    {
+        question: "How do I change my password?",
+        answer: "You can change your password by going to the settings page and clicking on the 'Change Password' button."
+    },
+    {
+        question: "What is the refund policy?",
+        answer: "We offer a 30-day money-back guarantee. If you are not satisfied with our service, you can request a refund within 30 days of your purchase."
+    },
+    {
+        question: "How do I contact support?",
+        answer: "You can contact support by going to the contact page and filling out the contact form. We will get back to you as soon as possible."    
+    },
+    {
+        question: "When do I recieve a password ordered by letter?",
+        answer: "You will receive a password ordered by letter within 5-7 business days. If you do not receive it within this time frame, please contact support."
+    }
+]
+
+export default faq
